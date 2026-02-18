88c9071407f20a6cde1dc0d5b57939934df33b7a
BalakrishnaSM <balakrishnasm45@gmail.com>
2026-02-18 11:16:47 +0530

    Second Commit

---
+++ js5.js
@@ -0,0 +1,28 @@
+// // Slice can take negative indexing whereas substring won't take negative indexing
+
+// let a="Hello Wolrd";
+
+// // let y = a.substring(4,8);
+// // console.log(y);
+
+// let x = 123456789
+
+// console.log(x)
+// // console.log(String(x))
+// console.log(typeof(x))
+// let y=(x.toString())
+// console.log(typeof(y))
+
+
+let a = "ravishastry"
+let company = "dheecodinglab"
+let address = "banglore"
+
+let y=(a.slice(0,6)+company+a.slice(6))
+
+let x=(y.slice(0,12)+address+y.slice(12))
+
+let z=(x+x+x+x+x)
+
+
+a=console.log(z.slice(0,79)+a.toUpperCase()+z.slice(79))
