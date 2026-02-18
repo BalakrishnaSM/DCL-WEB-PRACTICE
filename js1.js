960af426c86b9aed97e09b50e81d2c68cb321fff
BalakrishnaSM <balakrishnasm45@gmail.com>
2026-02-18 10:11:55 +0530

    Initial Commit

---
+++ js1.js
@@ -0,0 +1,34 @@
+        // let a="true";
+        // console.log(a);
+        // console.log(typeof(a))
+
+        // let x=Number(a)
+        // console.log(x)
+        // console.log(typeof(x))
+
+
+// let a=100;
+// console.log(a)
+// console.log(typeof(a))
+
+// let x=String(a)
+// console.log(x)
+// console.log(typeof(x))
+
+// let a=1001.10001
+
+// let x=Number.parseInt(a)
+// console.log(x)
+
+// let a=1001.10001
+
+// let x=Number.parseFloat(a)
+// console.log(x)
+
+// let a = None
+
+// console.log(Boolean(a))
+
+let a=100
+
+console.log(Number.parseFloat(a))
---
+++ js4.js
@@ -0,0 +1,56 @@
+// let a=100
+
+// console.log(Number.parseFloat(a))
+
+
+// NULL
+
+// let x = null;
+// console.log(x)
+// console.log(typeof(x))
+
+// let x = null;
+// x='lion';
+// console.log(x)
+// console.log(typeof(x))
+
+// let y = undefined;
+// console.log(y)
+// console.log(typeof(y))
+
+// let y = undefined;
+// y='lion';
+// console.log(y)
+// console.log(typeof(y))
+
+// let y=undefined
+// y=100
+// console.log(y)
+// console.log(typeof(y))
+
+let a = "Hello";
+let y= "World";
+
+console.log(a)
+console.log(typeof(a))
+// console.log(a.charAt(0))
+
+// console.log(a.concat(" ").concat(y));
+
+// console.log(a.endsWith('o'))
+
+// console.log(a.startsWith('H'))
+
+// console.log(a.includes('ll'))
+
+// console.log(a.indexOf('l'))
+
+// console.log(a.lastIndexOf('l'))
+
+// console.log(a.padEnd(10,'y'))
+
+// console.log(a.padStart(10,'x'))
+// console.log(a.repeat(5))
+
+console.log(a.slice(0,3));
+
